17e3d7da3a091ebc22a50cbe40e8245eeaa77f9a
Denys Filichkin <denys.philichkin@gmail.com>
2021-06-29 18:16:45 +0300

    tasks

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const navItemEl = document.querySelectorAll('.item h2, li');
+
+console.log(navItemEl);
+
+// const titleEl = document.querySelectorAll('categories h2');
+// console.log(titleEl.textContent);
