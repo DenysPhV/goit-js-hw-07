be89cfba692e5588870114cb64afda0a4cfdfa7f
Denys Filichkin <denys.philichkin@gmail.com>
2021-06-30 21:55:48 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,28 @@
-const navItemEl = document.querySelectorAll('.item h2, li');
+// const navItemEl = document.querySelectorAll('.item h2, li');
 
-console.log(navItemEl);
+// console.log(navItemEl);
 
 // const titleEl = document.querySelectorAll('categories h2');
 // console.log(titleEl.textContent);
+// const titleEl = document.querySelectorAll('h2');
+// console.log(...titleEl.textContent);
+
+// const menuItemsByTagName = document.querySelectorAll('.item');
+// console.log(menuItemsByTagName);
+
+function calculateDiscount(years, paymentMethod, returnedOrder) {
+  let discount = 0;
+  if (years >= 1) {
+    if (returnedOrder) {
+      discount = 0;
+      return discount;
+    }
+    if (paymentMethod === 'MasterCard') {
+      discount = 0.1;
+    }
+    if (getCustomerBirthdayDate() === getCurrentDate()) {
+      discount = 0.5;
+    }
+  }
+  return discount;
+}
