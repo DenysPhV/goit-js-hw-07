2956f5ba6fdee21fab64ed3fe63e577d698f7d32
Denys <77440293+DenysPhV@users.noreply.github.com>
2021-06-30 23:19:06 +0300

    update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,4 @@
-// const navItemEl = document.querySelectorAll('.item h2, li');
 
-// console.log(navItemEl);
-
-// const titleEl = document.querySelectorAll('categories h2');
-// console.log(titleEl.textContent);
-// const titleEl = document.querySelectorAll('h2');
-// console.log(...titleEl.textContent);
-
-// const menuItemsByTagName = document.querySelectorAll('.item');
-// console.log(menuItemsByTagName);
 
 function calculateDiscount(years, paymentMethod, returnedOrder) {
   let discount = 0;
